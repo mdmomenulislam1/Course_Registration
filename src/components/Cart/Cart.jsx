fc1c37109d7e645998283fcd282399ca0dd7e5de
mdmomenulislam1 <mdmomenulislam1@gmail.com>
2023-09-15 10:59:00 +0600

    Some issues fixed

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -8,21 +8,21 @@ const Cart = ({selectedCourses, remaining, totalTakenCredit, totalPayablePrice})
     <div>
       <h1 className="text-center mb-3 text-3xl font-bold"> Cart Section </h1>
       <div className="text-1xl font-bold mb-3">
-        <h1 className="text-blue-700 ">Credit Hour Remaining {remaining} hr</h1>
+        <h1 className="text-blue-700 ">Credit Hour Remaining  { remaining} hr</h1>
       </div>
       <hr />
       <div className=" my-3">
         <h1 className="text-2xl font-bold mb-2">Course Name: </h1>
 
         {selectedCourses.map((course) => (
-        <li key={course.id} className="list-none text-[15px] font-semibold">{selectedCourses.length}. {course.course_name}</li>
+        <li key={course.id} className="list-disc text-[15px] font-semibold">{course.course_name}</li>
       ))}
         <hr className="my-5" />
         <h1 className="text-2xl font-bold">Total Course: {selectedCourses.length}</h1>
         <hr className="my-5" />
-        <h1 className="text-2xl font-bold">Total Credit:{totalTakenCredit}</h1>
+        <h1 className="text-2xl font-bold">Total Credit: { totalTakenCredit}</h1>
         <hr className="my-5" />
-        <h1 className="text-2xl font-bold">Total Price:{totalPayablePrice.toFixed(2)}</h1>
+        <h1 className="text-2xl font-bold">Total Price: $ { totalPayablePrice.toFixed(2)}</h1>
         <hr className="my-5" />
       </div>
     </div>
